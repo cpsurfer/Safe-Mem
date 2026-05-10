60256cb8aa459d15ea2409552b3d4f1b93888171
Rahul <firstrahul39@gmail.com>
2026-05-10 14:07:30 +0000

    Refactor: Added C/Python compatibility to safemem.h and updated shared library logic

--- include/safemem.h
+++ include/safemem.h
@@ -1,18 +1,27 @@
-//Copyright (c) 2026 cpsurfer[Rahul]. All rights reserved.
+// Copyright (c) 2026 cpsurfer[Rahul]. All rights reserved.
 #ifndef SAFEMEM_H
 #define SAFEMEM_H
 
-#include <cstddef> 
-void* map_memory(size_t size);
+#include <stddef.h> 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void* safemem(size_t size);
 
-// Our custom allocator function
+void safe_free(void* ptr);
+
+void* map_memory(size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
 inline void* safe_malloc(size_t size) {
     return safemem(size);
 }
-
-// Frees the memory pointed to by ptr
-void safe_free(void* ptr);
-
 #endif
+
+#endif 
